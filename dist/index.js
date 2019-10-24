704c44849914c18026a162c85d291c40752e9602
Kazuya Takei <attakei@gmail.com>
2019-10-24 14:48:44 +0900

    Build target is es2017

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var js_1 = __importDefault(require("@bugsnag/js"));
+const js_1 = __importDefault(require("@bugsnag/js"));
 /**
  * Configure bugsnag client and prepare wrapper functions per any events.
  *
  * @param apiKey Bugsnag API key
  */
-exports.configureBugsnag = function (apiKey) {
+exports.configureBugsnag = (apiKey) => {
     return {
         bugsnagClient: js_1.default(apiKey),
     };
