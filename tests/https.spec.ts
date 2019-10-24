d66009fb4e2551fb9dcdd41563bd148597634226
Kazuya Takei <attakei@gmail.com>
2019-10-24 13:57:08 +0900

    Add error throwing case

--- tests/https.spec.ts
+++ tests/https.spec.ts
@@ -1,4 +1,4 @@
-import bugsnag from '@bugsnag/js';
+import bugsnag, { Bugsnag } from '@bugsnag/js';
 import { https } from 'firebase-functions';
 import * as t from '../src/functions/https';
 
@@ -6,10 +6,25 @@ const stubRegular: t.HttpsCallable<{}, boolean> = (data, ctx) => {
   return true;
 };
 
-describe('https functions', () => {
+const stubThrowError: t.HttpsCallable<{}, boolean> = (data, ctx) => {
+  throw new Error('Stub-error')
+};
+
+describe('https callable functions', () => {
   it('error is not thrown', () => {
     const client = bugsnag('mock-api-key');
     const wrapped = t.httpsOnCallWrapper(client, stubRegular);
     expect(wrapped({}, {} as https.CallableContext)).toBeTruthy();
+  });
+
+  it('catch error', () => {
+    const mockedClient = {
+      notify: (error: Bugsnag.NotifiableError) => error,
+    }
+    const mockedNotify = jest.spyOn(mockedClient, 'notify');
+    mockedNotify.mockImplementation((error: Bugsnag.NotifiableError) => error);
+    const wrapped = t.httpsOnCallWrapper(mockedClient as Bugsnag.Client, stubThrowError);
+    expect(() => wrapped({}, {} as https.CallableContext)).toThrowError(Error)
+    expect(mockedNotify).toBeCalled();
   });
 });
